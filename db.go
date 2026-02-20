4cba91fcb08dca548d7a3fc7e844c0bc357cf393
Parth Srivastav <parthsrivastav.00@gmail.com>
2026-02-20 18:49:33 +0530

    refactor: made memUsed atomic, related changes

--- db.go
+++ db.go
@@ -3,6 +3,7 @@ package main
 import (
 	"log"
 	"sync"
+	"sync/atomic"
 	"time"
 )
 
@@ -11,8 +12,8 @@ import (
 // Methods on RedisDb are thread-safe for now.
 type RedisDb struct {
 	store   map[string]*Item
-	mu      sync.Mutex
-	memUsed uint64 // memUsed is approximate memory usage of the database in bytes across
+	rwm     sync.RWMutex
+	memUsed atomic.Uint64 // memUsed is approximate memory usage of the database in bytes across
 }
 
 // NewRedisDb returns an initialized empty database.
@@ -22,30 +23,38 @@ func NewRedisDb() *RedisDb {
 	}
 }
 
+// MemUsed returns the approximate memory usage of the database in bytes.
+// It is an atomic operation, hence can be read from multiple goroutines.
+func (rdb *RedisDb) MemUsed() uint64 {
+	return rdb.memUsed.Load()
+}
+
 // Set stores a key-value pair in the database. If the key already existed in
 // the database, its memory usage is subtracted before overwriting. Set is
 // thread-safe.
 func (rdb *RedisDb) Set(key, val string) {
-	rdb.mu.Lock()
-	defer rdb.mu.Unlock()
+	rdb.rwm.Lock()
+	defer rdb.rwm.Unlock()
 
 	if old, ok := rdb.store[key]; ok {
-		rdb.memUsed -= old.approxMemUsage(key)
+		prev := rdb.memUsed.Load()
+		curr := prev - old.approxMemUsage(key)
+		rdb.memUsed.Store(curr)
 	}
-	item := Item{Value: val}
+	item := &Item{Value: val}
 	imem := item.approxMemUsage(key)
 
-	rdb.memUsed += imem
-	rdb.store[key] = &item
-	log.Printf("set key=%q, memory usage=%d bytes", key, rdb.memUsed)
+	rdb.memUsed.Add(imem)
+	rdb.store[key] = item
+	log.Printf("set key=%q, memory usage=%d bytes", key, rdb.memUsed.Load())
 }
 
 // Get returns the (val, true) for the given key, or (nil, false) if the key
 // does not exist. Get updates LastAccessed and AccessCount on the Item for
 // LRU/LFU tracking. Get is thread-safe.
 func (rdb *RedisDb) Get(key string) (*Item, bool) {
-	rdb.mu.Lock()
-	defer rdb.mu.Unlock()
+	rdb.rwm.Lock()
+	defer rdb.rwm.Unlock()
 
 	item, ok := rdb.store[key]
 	if !ok {
@@ -63,14 +72,19 @@ func (rdb *RedisDb) Get(key string) (*Item, bool) {
 // Delete removes the key from the underlying store and updates the memory
 // usage, returns early if key not exists. Delete is thread-safe.
 func (rdb *RedisDb) Delete(key string) {
-	rdb.mu.Lock()
-	defer rdb.mu.Unlock()
+	rdb.rwm.Lock()
+	defer rdb.rwm.Unlock()
 
 	item, ok := rdb.store[key]
 	if !ok {
 		return
 	}
-	rdb.memUsed -= item.approxMemUsage(key)
+	prev := rdb.memUsed.Load()
+	curr := prev - item.approxMemUsage(key)
+	rdb.memUsed.Store(curr)
 	delete(rdb.store, key)
-	log.Printf("deleted key=%q, memory usage=%d bytes", key, rdb.memUsed)
+	log.Printf(
+		"deleted key=%q, memory usage=%d bytes",
+		key, rdb.memUsed.Load(),
+	)
 }
