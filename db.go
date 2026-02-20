54a0182b95c7538d53d329533818ccfc7e4c73cc
Parth Srivastav <parthsrivastav.00@gmail.com>
2026-02-20 17:39:01 +0530

    feat: database operations set, get, delete

--- db.go
+++ db.go
@@ -1,17 +1,76 @@
 package main
 
 import (
+	"log"
 	"sync"
+	"time"
 )
 
-type Database struct {
-	rwm    sync.RWMutex
-	memory int64
-	store  map[string]*Item
+// RedisDb represents a Redis database, an in-memory key-value store; instance
+// must not be copied after first use because sync.Mutex must not be copied.
+// Methods on RedisDb are thread-safe for now.
+type RedisDb struct {
+	store   map[string]*Item
+	mu      sync.Mutex
+	memUsed uint64 // memUsed is approximate memory usage of the database in bytes across
 }
 
-func NewDb() *Database {
-	return &Database{
+// NewRedisDb returns an initialized empty database.
+func NewRedisDb() *RedisDb {
+	return &RedisDb{
 		store: make(map[string]*Item),
 	}
+}
+
+// Set stores a key-value pair in the database. If the key already existed in
+// the database, its memory usage is subtracted before overwriting. Set is
+// thread-safe.
+func (rdb *RedisDb) Set(key, val string) {
+	rdb.mu.Lock()
+	defer rdb.mu.Unlock()
+
+	if old, ok := rdb.store[key]; ok {
+		rdb.memUsed -= old.approxMemUsage(key)
+	}
+	item := Item{Value: val}
+	imem := item.approxMemUsage(key)
+
+	rdb.memUsed += imem
+	rdb.store[key] = &item
+	log.Printf("set key=%q, memory usage=%d bytes", key, rdb.memUsed)
+}
+
+// Get returns the (val, true) for the given key, or (nil, false) if the key
+// does not exist. Get updates LastAccessed and AccessCount on the Item for
+// LRU/LFU tracking. Get is thread-safe.
+func (rdb *RedisDb) Get(key string) (*Item, bool) {
+	rdb.mu.Lock()
+	defer rdb.mu.Unlock()
+
+	item, ok := rdb.store[key]
+	if !ok {
+		return nil, false
+	}
+	item.AccessCount++
+	item.LastUsedAt = time.Now()
+	log.Printf(
+		"key=%q accessed %d times, last used at=%v",
+		key, item.AccessCount, item.LastUsedAt,
+	)
+	return item, true
+}
+
+// Delete removes the key from the underlying store and updates the memory
+// usage, returns early if key not exists. Delete is thread-safe.
+func (rdb *RedisDb) Delete(key string) {
+	rdb.mu.Lock()
+	defer rdb.mu.Unlock()
+
+	item, ok := rdb.store[key]
+	if !ok {
+		return
+	}
+	rdb.memUsed -= item.approxMemUsage(key)
+	delete(rdb.store, key)
+	log.Printf("deleted key=%q, memory usage=%d bytes", key, rdb.memUsed)
 }
